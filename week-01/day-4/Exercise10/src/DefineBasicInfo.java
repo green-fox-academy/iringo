6045d7ea1c668a52007ece391cb756e1943b20ab
Iringo <iringobalazs@gmail.com>
2018-08-02 13:53:40 +0200

    Exercise 10 done.

---
+++ week-01/day-4/Exercise10/src/DefineBasicInfo.java
@@ -0,0 +1,25 @@
+public class DefineBasicInfo {
+
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+    String name = "Iringó";
+    int age = 28;
+    double height = 180;
+    boolean marriage = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(marriage);
+
+
+
+    }
+
+
+}
