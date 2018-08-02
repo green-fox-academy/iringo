36ef13d75e3601a0742f9c9cbc273d0006e0f017
Iringo <iringobalazs@gmail.com>
2018-08-02 12:11:00 +0200

    Exercise 03 done.

---
+++ week-01/day-4/Exercise03/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello, István!");
+        System.out.println("Hello, Krisztián!");
+        System.out.println("Hello, Tamás!");
+    }
+}
