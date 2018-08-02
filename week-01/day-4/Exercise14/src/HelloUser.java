bbb1c69b60834d87ea6b32a61135a7980909fa77
Iringo <iringobalazs@gmail.com>
2018-08-02 14:51:19 +0200

    feat (exercise 14): task finish

---
+++ week-01/day-4/Exercise14/src/HelloUser.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HelloUser {
+
+    public static void main(String[] args) {
+
+        Scanner name = new Scanner(System.in);
+        // Modify this program to greet user instead of the World!
+        // The program should ask for the name of the user
+        System.out.println("Hello, User!");
+        String userInput1 = name.nextLine();
+    }
+
+
+
+}
