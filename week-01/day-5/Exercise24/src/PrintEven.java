c22c18211376ef3749108b2dd658ec1d9c52e9b9
Iringo <iringobalazs@gmail.com>
2018-08-03 11:26:34 +0200

    feat (exercise 24): the correct finish task

--- week-01/day-5/Exercise24/src/PrintEven.java
+++ week-01/day-5/Exercise24/src/PrintEven.java
@@ -5,7 +5,9 @@ public class PrintEven {
     public static void main(String[] args) {
 
         for (int i = 0; i < 501; i++) {
-            System.out.println(i);
+            if (i % 2 == 0  ) {
+                System.out.println(i);
+            }
         }
 
     }
