8f81137b7470404bc61570b6c47c00a71e9543a1
Iringo <iringobalazs@gmail.com>
2018-08-03 11:24:56 +0200

    feat (exercise 24): finish task

---
+++ week-01/day-5/Exercise24/src/PrintEven.java
@@ -0,0 +1,12 @@
+public class PrintEven {
+
+    // Create a program that prints all the even numbers between 0 and 500
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 501; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
