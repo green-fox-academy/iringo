45ca53d38d97c4db5bf119a40ee2a9b4d4908728
Iringo <iringobalazs@gmail.com>
2018-08-05 19:23:42 +0200

    feat (exercise 12): finish task

---
+++ week-01/day-4/Exercise12/src/Cuboid.java
@@ -0,0 +1,26 @@
+
+    // Write a program that stores 3 sides of a cuboid as variables (doubles)
+    // The program should write the surface area and volume of the cuboid like:
+    //
+    // Surface Area: 600
+    // Volume: 1000
+
+
+
+public class Cuboid {
+
+    public static void main(String[] args) {
+
+        double a = 1;
+        double b = 2;
+        double c = 3;
+
+        double surface = 2 * (a * b + a * c + b * c);
+        double volume = a * b * c;
+
+        System.out.println("Surface area: " + surface);
+        System.out.println("Volume: " + volume);
+
+    }
+
+}
