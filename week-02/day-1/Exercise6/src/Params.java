f12e42b4f52b95e9feea4ebefc373744b0323feb
Iringo <iringobalazs@gmail.com>
2018-08-06 15:54:03 +0200

    feat (exercise params): finish task

---
+++ week-02/day-1/Exercise6/src/Params.java
@@ -0,0 +1,28 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `printParams`
+//   which prints the input String parameters
+// - It can have any number of parameters
+
+// Examples
+// printParams("first")
+// printParams("first", "second")
+// printParams("first", "second", "third", "fourth")
+// ...
+
+public class Params {
+
+    public static void main (String[] args) {
+
+        printParams("one", "two", "three");
+
+    }
+
+    public static void printParams(String... args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+}
