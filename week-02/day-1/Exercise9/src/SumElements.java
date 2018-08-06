a488fdcbb0c6ddaea894b3be103bc98fc6ac9617
Iringo <iringobalazs@gmail.com>
2018-08-06 16:46:15 +0200

    feat (exercise sum elements): finish task

---
+++ week-02/day-1/Exercise9/src/SumElements.java
@@ -0,0 +1,17 @@
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+import java.util.Arrays;
+
+public class SumElements {
+
+    public static void main (String[] args) {
+
+    int[] r = new int[] {54, 23, 66, 12};
+    int sum = r[2] + r[3];
+    System.out.println(sum);
+
+    }
+
+}
