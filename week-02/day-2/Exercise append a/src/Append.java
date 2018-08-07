4cd20009c53bf6968e0e4abe6a39a110328b0c1b
Iringo <iringobalazs@gmail.com>
2018-08-07 16:16:12 +0200

    feat (exercise append a): finish task

---
+++ week-02/day-2/Exercise append a/src/Append.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class Append {
+
+    public static void main(String[] args) {
+
+        List<String> far = Arrays.asList("bo", "anacond", "koal", "pand", "zebr");
+        // Create a method called "appendA()" that adds "a" to every string in the "far" list.
+        // The parameter should be a list.
+
+        System.out.println(appendA(far));
+
+    }
+
+    public static List appendA (List<String> list) {
+
+        List<String> outcome = new ArrayList<String>();
+        for (int i = 0; i <= list.size()-1; i++) {
+            outcome.add(list.get(i) + "a");
+        }
+        return (outcome);
+
+    }
+
+}
