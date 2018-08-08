1300175f37ad6400b0838c64b4c299e31fdbdeb5
Iringo <iringobalazs@gmail.com>
2018-08-08 15:35:19 +0200

    feat (exercise center box): finish task

---
+++ week-02/day-3/CenteredBoxes/src/Drawing.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static java.awt.image.ImageObserver.HEIGHT;
+import static java.awt.image.ImageObserver.WIDTH;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 1 parameter:
+        // the square size
+        // and draws a square of that size to the center of the canvas.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+
+        int x = 20;
+        for (int i = 0; i < 3; i++) {
+            squareDraw(graphics, x);
+            x = x + 20;
+        }
+    }
+
+    public static void squareDraw (Graphics graphics, int size) {
+        graphics.drawRect(WIDTH / 2 - size/2, HEIGHT / 2 - size/2, size, size);
+    }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
