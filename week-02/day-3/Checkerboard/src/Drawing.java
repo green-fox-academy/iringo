e5c3fce668a2f66b0bb2637c6f963505e07580e5
Iringo <iringobalazs@gmail.com>
2018-08-08 22:09:01 +0200

    feat (exercise: checkerboard): finish task

--- week-02/day-3/Checkerboard/src/Drawing.java
+++ week-02/day-3/Checkerboard/src/Drawing.java
@@ -4,45 +4,53 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Drawing {
-    public static void mainDraw(Graphics graphics) {
-        // Fill the canvas with a checkerboard pattern.
+    public class Drawing {
 
-        int x = 0;
-        int y = 0;
-        int size = 32;
+        public static void mainDraw(Graphics graphics) {
+            // Fill the canvas with a checkerboard pattern.
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                x = j * size;
-                y = i * size;
-                if ( (i % 2) == (j % 2) )
-                    graphics.setColor(Color.WHITE);
-                else
-                    graphics.setColor(Color.BLACK);
-                graphics.fillRect(x, y, size, size);
+            int size = 32;
+            int x = 0;
+            int y = 0;
+
+            for (int j = 0; j < 10; j++){
+                for (int i = 0; i < 10; i++) {
+                    if ((j % 2 == 0) && (i % 2 == 0)){
+                        graphics.setColor(Color.BLACK);
+                        graphics.fillRect(x, y, size, size);
+                    } else if ((j % 2 != 0) && (i % 2 != 0)) {
+                        graphics.setColor(Color.BLACK);
+                        graphics.fillRect(x, y, size, size);
+                    } else {
+                        graphics.setColor(Color.white);
+                        graphics.drawRect(x, y, size, size);
+                    }
+                    x = x + size;
+                }
+                x = 0;
+                y = y + size;
+            }
+
+        }
+
+        // Don't touch the code below
+        static int WIDTH = 320;
+        static int HEIGHT = 343;
+
+        public static void main(String[] args) {
+            JFrame jFrame = new JFrame("Drawing");
+            jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+            jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            jFrame.add(new ImagePanel());
+            jFrame.setLocationRelativeTo(null);
+            jFrame.setVisible(true);
+        }
+
+        static class ImagePanel extends JPanel {
+            @Override
+            protected void paintComponent(Graphics graphics) {
+                super.paintComponent(graphics);
+                mainDraw(graphics);
             }
         }
     }
-
-    // Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 343;
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        jFrame.add(new ImagePanel());
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-    }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
-    }
-}
