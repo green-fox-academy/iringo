31f86e2ab86a4759089a3e81f88a60220baef30c
Iringo <iringobalazs@gmail.com>
2018-08-09 17:31:58 +0200

    Task not yet finished

--- week-02/day-1/Exercise20/src/Unique.java
+++ week-02/day-1/Exercise20/src/Unique.java
@@ -20,8 +20,7 @@ public class Unique {
         Arrays.sort(array);
 
         int counter = 0;
-        for (int i = 0; i < array.length-1; i++) {
-            for (int j = i+1; j < array.length-1; j++) {
-                
+
+
 
 }
