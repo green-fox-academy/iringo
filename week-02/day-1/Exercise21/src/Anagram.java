5326b857812d9c183fa6a697804f23eb58300b9c
Iringo <iringobalazs@gmail.com>
2018-08-12 18:47:46 +0200

    feat: I am not sure why is it not working

---
+++ week-02/day-1/Exercise21/src/Anagram.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        // Create a function named is anagram following your current language's style guide.
+        // It should take two strings and return a boolean value depending on whether its an anagram or not.
+         String firstWord = "dog";
+         String secondWord = "god";
+         anagram(firstWord, secondWord);
+
+    }
+
+    public static boolean anagram (String first, String second){
+
+        if (first.length() != second.length()){
+           return false;
+        }
+
+       first = first.toLowerCase();
+       second = second.toLowerCase();
+       char[] sorted1 = first.toCharArray();
+       char[] sorted2 = second.toCharArray();
+       Arrays.sort(sorted1);
+       Arrays.sort(sorted2);
+
+        for (int i = 0; i < sorted1.length; i++) {
+            if (sorted1[i] != sorted2[i] ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
