8d72be0927ddeaba9b1013c9e51d689a26868765
Iringo <iringobalazs@gmail.com>
2018-08-12 18:21:30 +0200

    feat (exercise 20): finish task

--- week-02/day-1/Exercise20/src/Unique.java
+++ week-02/day-1/Exercise20/src/Unique.java
@@ -2,25 +2,26 @@ import java.util.Arrays;
 import java.util.*;
 
 public class Unique {
+
     public static void main(String[] args) {
-        //  Create a function that takes a list of numbers as a parameter
-        //  Returns a list of numbers where every number in the list occurs only once
 
-        //  Example
-        //System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
-        //  should print: `[1, 11, 34, 52, 61]`
+        System.out.println(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34}));
 
-        int[] unique = new int[] {1, 11, 34, 11, 52, 61, 1, 34};
-        sort(unique);
-        System.out.println(Arrays.toString(unique));
     }
 
-    public static int[] sort (int[] array) {
+    private static String unique(int[] list) {
 
-        Arrays.sort(array);
+        String sorted = "";
+        for (int i = 0; i < list.length; i++) {
+            String numberAsString = String.valueOf(list[i]);
+            if (!sorted.contains(numberAsString)) {
+                sorted += numberAsString;
+                sorted += " ";
+            }
 
-        int counter = 0;
-
+        }
+        return sorted;
+    }
 
 
 }
