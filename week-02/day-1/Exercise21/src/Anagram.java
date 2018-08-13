de3469b959bfe4563f99b562e25e799814a1bb21
Iringo <iringobalazs@gmail.com>
2018-08-13 22:00:56 +0200

    feat (exercise anagram): finish task

--- week-02/day-1/Exercise21/src/Anagram.java
+++ week-02/day-1/Exercise21/src/Anagram.java
@@ -5,32 +5,38 @@ public class Anagram {
     public static void main(String[] args) {
         // Create a function named is anagram following your current language's style guide.
         // It should take two strings and return a boolean value depending on whether its an anagram or not.
-         String firstWord = "dog";
-         String secondWord = "god";
-         anagram(firstWord, secondWord);
+        String firstWord = "dog";
+        String secondWord = "god";
+        System.out.println(anagram(firstWord, secondWord));
+        ;
 
     }
 
-    public static boolean anagram (String first, String second){
+    public static boolean anagram(String first, String second) {
 
-        if (first.length() != second.length()){
-           return false;
+        boolean returnValue = true;
+
+        if (first.length() != second.length()) {
+            returnValue = false;
         }
 
-       first = first.toLowerCase();
-       second = second.toLowerCase();
-       char[] sorted1 = first.toCharArray();
-       char[] sorted2 = second.toCharArray();
-       Arrays.sort(sorted1);
-       Arrays.sort(sorted2);
+        first = first.toLowerCase();
+        second = second.toLowerCase();
+        char[] sorted1 = first.toCharArray();
+        char[] sorted2 = second.toCharArray();
+        Arrays.sort(sorted1);
+        Arrays.sort(sorted2);
 
         for (int i = 0; i < sorted1.length; i++) {
-            if (sorted1[i] != sorted2[i] ) {
-                return false;
+            if (sorted1[i] != sorted2[i]) {
+                returnValue = false;
+            } else {
+                returnValue = true;
             }
         }
 
-        return true;
+        return returnValue;
     }
+
 
 }
