36be6b8fc446705141bf8305156af30ef20ccbee
Iringo <iringobalazs@gmail.com>
2018-08-15 16:48:14 +0200

    feat (exercise dice roll): finish task

--- week-03/Day-3/DiceSet/src/DiceSet.java
+++ week-03/Day-3/DiceSet/src/DiceSet.java
@@ -1,24 +1,25 @@
 public class DiceSet {
 
-    // You have a `DiceSet` class which has a list for 6 dices
+    //You have a `DiceSet` class which has a list for 6 dices
 // You can roll all of them with roll()
 // Check the current rolled numbers with getCurrent()
 // You can reroll with reroll()
 // Your task is to roll the dices until all of the dices are 6
+
     int[] dices = new int[6];
 
-    public int[] roll() {
+    int[] roll() {
         for (int i = 0; i < dices.length; i++) {
             dices[i] = (int) (Math.random() * 6) + 1;
         }
         return dices;
     }
 
-    public int[] getCurrent() {
+    int[] getCurrent() {
         return dices;
     }
 
-    public int getCurrent(int i) {
+    int getCurrent(int i) {
         return dices[i];
     }
 
--- week-03/Day-3/DiceSet/src/Main.java
+++ week-03/Day-3/DiceSet/src/Main.java
@@ -2,15 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
         DiceSet diceSet = new DiceSet();
-        for (int i = 0; i < 6; i++) {
-            diceSet.getCurrent();
-            diceSet.roll();
-            diceSet.getCurrent();
-            diceSet.getCurrent(5);
-            diceSet.reroll();
-            diceSet.getCurrent();
-            diceSet.reroll(4);
-            diceSet.getCurrent();
+
+        diceSet.roll();
+
+        for (int i = 0; i < diceSet.dices.length; i++) {
+            if (diceSet.getCurrent(i) != 6) {
+                do {
+                    diceSet.reroll(i);
+                } while ((diceSet.getCurrent(i)) != 6);
+            }
+        }
+
+        for (int j = 0; j < diceSet.dices.length; j++) {
+            System.out.println(diceSet.dices[j]);
         }
 
     }
