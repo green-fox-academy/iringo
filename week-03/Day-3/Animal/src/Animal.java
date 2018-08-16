8495e64de4e3ffdfec02e87902ce26af9567d180
Iringo <iringobalazs@gmail.com>
2018-08-16 11:44:31 +0200

    feat (exercise animal): finish task

---
+++ week-03/Day-3/Animal/src/Animal.java
@@ -0,0 +1,34 @@
+// Every animal has a hunger value, which is a whole number
+//Every animal has a thirst value, which is a whole number
+//when creating a new animal object these values are created with the default 50 value
+//Every animal can eat() which decreases their hunger by one
+//Every animal can drink() which decreases their thirst by one
+//Every animal can play() which increases both by one
+
+
+public class Animal {
+
+    int hunger;
+    int thirst;
+
+    Animal (int hunger, int thirst) {
+        this.hunger = hunger;
+        this.thirst = thirst;
+    }
+
+    public int eat() {
+        this.hunger --;
+        return hunger;
+    }
+
+    public int drink() {
+        this.thirst --;
+        return thirst;
+    }
+
+    public void play(){
+        this.hunger ++;
+        this.thirst ++;
+    }
+
+}
---
+++ week-03/Day-3/Animal/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal1 = new Animal(50, 50);
+
+        for (int i = 0; i < 5; i++) {
+            animal1.eat();
+        }animal1.eat();
+        System.out.println(animal1.hunger);
+
+        for (int i = 0; i < 8; i++) {
+            animal1.drink();
+        }
+        System.out.println(animal1.thirst);
+
+        for (int i = 0; i < 4; i++) {
+            animal1.play();
+        }
+        System.out.println("Hunger: " + Integer.toString(animal1.hunger) + " " + "Thirst: " + Integer.toString(animal1.thirst));
+
+    }
+
+}
