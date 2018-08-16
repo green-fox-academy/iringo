4362976280b21a4c9e6dab5601a94e975dfe6775
Iringo <iringobalazs@gmail.com>
2018-08-16 15:29:13 +0200

    feat (exercise sum digit): finish task

---
+++ week-03/Day-4/03SumDigit/src/Main.java
@@ -0,0 +1,24 @@
+// Given a non-negative int n, return the sum of its digits recursively (no loops).
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDigits(12345));
+
+    }
+
+    public static int sumOfDigits(int n) {
+
+        int sum = 0;
+        if (n == 0) {
+            return 0;
+        } else {
+            return (sum = (n % 10) + sumOfDigits(n / 10));
+        }
+
+    }
+}
