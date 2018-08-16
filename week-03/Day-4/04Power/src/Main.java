ea540d7533541f5cefa7344ec61fac07c9a555d3
Iringo <iringobalazs@gmail.com>
2018-08-16 16:16:31 +0200

    feat (exercise power): finish task

---
+++ week-03/Day-4/04Power/src/Main.java
@@ -0,0 +1,23 @@
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int base;
+        int n;
+        System.out.println(powerN(3, 3));
+
+    }
+
+    public static int powerN(int base, int n) {
+
+        int output = 0;
+        if (n != 0){
+            return (base * (powerN(base, n-1)));
+        } else {
+            return 1;
+        }
+    }
+}
