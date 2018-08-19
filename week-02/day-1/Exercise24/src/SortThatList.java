9cd10711413763cee99e3774ab5b1ab97b74fa05
Iringo <iringobalazs@gmail.com>
2018-08-19 10:55:30 +0200

    feat (exercise sort that list): finish task with correct solution

--- week-02/day-1/Exercise24/src/SortThatList.java
+++ week-02/day-1/Exercise24/src/SortThatList.java
@@ -8,24 +8,43 @@
 //    //  should print [34, 24, 12, 9, 5]
 
 import java.util.*;
+import java.util.Arrays;
 
 public class SortThatList {
 
     public static void main(String[] args) {
 
-        System.out.println(bubble(new int[] {34, 12, 24, 9, 5}));
+        //  Create a function that takes a list of numbers as parameter
+        //  Returns a list where the elements are sorted in ascending numerical order
+        //  Make a second boolean parameter, if it's `true` sort that list descending
+
+        //  Example:
+        //System.out.println(bubble(new int[] {34, 12, 24, 9, 5}));
+        //  should print [5, 9, 12, 24, 34]
+        //System.out.println(advancedBubble(new int[] {34, 12, 24, 9, 5}, true));
+        //  should print [34, 24, 12, 9, 5]
+
+
+        Integer[] inputList = new Integer[]{34, 12, 24, 9, 5};
+        boolean input = true;
+        bubble(new int[]{34, 12, 24, 9, 5});
+        advancedBubble(inputList, input);
 
     }
 
-    public static List<Integer> bubble (int[] inputList){
+    public static void bubble(int[] inputList) {
 
         Arrays.sort(inputList);
-        Arrays.asList(inputList);
-        List<Integer> output = new ArrayList<Integer> (Arrays.t(inputList));
+        System.out.println(Arrays.toString(inputList));
 
-        List<Integer> output = new ArrayList<Integer> ()
-        return output;
     }
 
+    public static void advancedBubble(Integer[] inputList, boolean input) {
+
+        if (input == true) {
+            Arrays.sort(inputList, Collections.reverseOrder());
+        }
+        System.out.println(Arrays.toString(inputList));
+    }
 
 }
