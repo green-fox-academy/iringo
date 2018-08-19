cba6593242e13c5c4b416bf3a79b1846ee6aa24d
Iringo <iringobalazs@gmail.com>
2018-08-19 17:31:37 +0200

    feat ( exercise Student Counter): class cast exception problem

--- week-02/day-2/HashMap2/src/StudenCounter.java
+++ week-02/day-2/HashMap2/src/StudenCounter.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class StudenCounter{
-    public static void main(String... args){
+public class StudenCounter {
+    public static void main(String... args) {
         ArrayList<Map<String, Object>> map = new ArrayList<Map<String, Object>>();
 
         // Given this list of hashmaps...
@@ -10,40 +10,58 @@ public class StudenCounter{
         row0.put("name", "Theodor");
         row0.put("age", 9.5);
         row0.put("candies", 2);
-        map.add( row0 );
+        map.add(row0);
 
         Map<String, Object> row1 = new HashMap<String, Object>();
         row1.put("name", "Paul");
         row1.put("age", 10);
         row1.put("candies", 1);
-        map.add( row1 );
+        map.add(row1);
 
         Map<String, Object> row2 = new HashMap<String, Object>();
         row2.put("name", "Mark");
         row2.put("age", 7);
         row2.put("candies", 3);
-        map.add( row2 );
+        map.add(row2);
 
         Map<String, Object> row3 = new HashMap<String, Object>();
         row3.put("name", "Peter");
         row3.put("age", 12);
         row3.put("candies", 5);
-        map.add( row3 );
+        map.add(row3);
 
         Map<String, Object> row4 = new HashMap<String, Object>();
         row4.put("name", "Olaf");
         row4.put("age", 12);
         row4.put("candies", 7);
-        map.add( row4 );
+        map.add(row4);
 
         Map<String, Object> row5 = new HashMap<String, Object>();
         row5.put("name", "George");
         row5.put("age", 3);
         row5.put("candies", 2);
-        map.add( row5 );
+        map.add(row5);
 
         // Display the following things:
         //  - Who has got more candies than 4 candies
         //  - Sum the age of people who have lass than 5 candies
+
+        String names = "";
+        for (Map<String, Object> entry : map) {
+            if ((int) entry.get("candies") > 4) {
+                names += entry.get("name");
+                names += " ";
+            }
+        }
+        System.out.println("People who have got more than four candies are: " + names);
+
+        Integer sumOfAge = 0;
+        for (Map<String, Object> entry : map) {
+            if ((int) entry.get("candies") < 5) {
+                sumOfAge += (Integer) (entry.get("age"));
+            }
+        }
+        System.out.println("The sum age of people who have less than 5 candies: " + sumOfAge);
+
     }
 }
