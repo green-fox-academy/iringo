871337844d0a8b68025cf9ac195fc3dac8ee164c
Iringo <iringobalazs@gmail.com>
2018-08-20 16:55:40 +0200

    feat (exercise Sharpie set): finisht task

--- week-03/Day-3/SharpieSet/src/SharpieSet.java
+++ week-03/Day-3/SharpieSet/src/SharpieSet.java
@@ -1,35 +1,32 @@
 // Reuse your Sharpie class
 //Create SharpieSet class
 //it contains a list of Sharpie
-    //countUsable() -> sharpie is usable if it has ink in it
-    //removeTrash() -> removes all unusable sharpies
+//countUsable() -> sharpie is usable if it has ink in it
+//removeTrash() -> removes all unusable sharpies
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SharpieSet{
+public class SharpieSet {
 
     ArrayList<Sharpie> list = new ArrayList<Sharpie>();
 
     public int countUsable() {
-
         int counter = 0;
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).inkAmount > 0) {
-                counter ++;
+                counter++;
             }
         }
         return counter;
     }
 
     public void removeTrash() {
-
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).inkAmount <= 0) {
                 list.remove(i);
             }
         }
-
     }
 
 }
