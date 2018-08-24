f1f313bc4caf1270d440339179f3cd4259c015df
Iringo <iringobalazs@gmail.com>
2018-08-24 10:55:07 +0200

    feat (exercise sharpie testing): finish exercise

---
+++ week-04/day-4/Sharpie/Tests/SharpieTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SharpieTest {
+
+    Sharpie sharpie;
+
+    @Before
+    public void createSharpie() {
+        sharpie = new Sharpie("red", 10);
+    }
+
+    @Test
+    public void useWithCorrectOutcome() {
+        assertEquals(sharpie.use(40), 60, 0.05);
+    }
+
+    @Test
+    public void useWithWrongOutcome() {
+        assertEquals(sharpie.use(40), 50, 0.05);
+    }
+
+    @Test
+    public void useWithNullOutcome() {
+        assertEquals(sharpie.use(60), 0, 0.05);
+    }
+}
