3edf5df3d27c8eade71a7a9e6e57c871bdeea8d6
Iringo <iringobalazs@gmail.com>
2018-09-11 09:54:49 +0200

    feat (exercise hello-others): finished

---
+++ week-06/day1/hello-others/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+  // Greet 3 of your classmates with this program, in three separate lines
+  // like:
+  //
+  // Hello, Esther!
+  // Hello, Mary!
+  // Hello, Joe!
+  printf("Hello Andras! \n");
+  printf("Hello Balazs! \n");
+  printf("Hello Gergo! \n");
+
+}
