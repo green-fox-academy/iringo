c75b00bf9026b1252e70197850d19c2aed28a6d2
Iringo <iringobalazs@gmail.com>
2018-09-11 16:41:55 +0200

    feat (exercise: define basic info) task finished

---
+++ week-06/day1/defineBasicInfo/main.c
@@ -0,0 +1,19 @@
+main()
+{
+    // Define several things as a variable then print their values
+    // First letter of your name as a char
+    // Your age as an integer
+    // Your height in meters as a double
+    // Your favourite positive number as an unsigned int
+
+    char firstLetter = 'i';
+    int age = 28;
+    double height = 1.80;
+    unsigned int favNumber = 23;
+
+    printf("My name's first letter is: %c \n", firstLetter);
+    printf("I am %d years old. \n", age);
+    printf("I am %.2f meters high. \n", height);
+    printf("My favourite positive number is: %u", favNumber);
+
+}
