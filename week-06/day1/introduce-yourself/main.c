9a1e0654a53b273b9b2a9f8c751308976ebd304a
Iringo <iringobalazs@gmail.com>
2018-09-11 10:00:56 +0200

    feat (exercise introduce yourself) task finished

---
+++ week-06/day1/introduce-yourself/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+  // Write a program that prints a few details to the terminal window about you
+  // It should print each detail to a new line.
+  //  - Your name
+  //  - Your age
+  //  - Your height in meters (as a decimal fraction)
+  //
+  //  Example output:
+  //  John Doe
+  //  31
+  //  1.87
+
+  printf("Iringo Ignacz \n");
+  printf("%d \n", 28);
+  printf("%.2f \n", 1.80);
+  return 0;
+}
