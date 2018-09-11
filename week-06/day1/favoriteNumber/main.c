9368188767e1b8ca321f574d9a3f81727fc78f73
Iringo <iringobalazs@gmail.com>
2018-09-11 15:40:13 +0200

    feat (exerise: favourite number): finished

---
+++ week-06/day1/favoriteNumber/main.c
@@ -0,0 +1,10 @@
+main()
+{
+    // Store your favorite number in a variable (as a number)
+    // And print it like this: "My favorite number is: 8"
+
+    int myNumber = 23;
+    printf("My favourite number is: %d \n", myNumber);
+
+
+}
