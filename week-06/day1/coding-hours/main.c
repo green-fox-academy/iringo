eb5ee7f34f404d8d9935e10864655e91ac30a82a
Iringo <iringobalazs@gmail.com>
2018-09-11 10:17:37 +0200

    feat (exercise coding hours) task finished

---
+++ week-06/day1/coding-hours/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+  // An average Green Fox attendee codes 6 hours daily
+  // The semester is 17 weeks long
+  //
+  // Print how many hours is spent with coding in a semester by an attendee,
+  // if the attendee only codes on workdays.
+  //
+  // Print the percentage of the coding hours in the semester if the average
+  // work hours weekly is 52
+
+    printf("An average Green Fox attendee codes: %d hours in average. \n", 17 * 5 * 6);
+    printf("An average Green Fox attendee codes: %d hours in average. \n", 17 * 52);
+
+  return 0;
+
+}
