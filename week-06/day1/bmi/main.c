c513990ec3fad37c19f3a9b48b1216688bc7e5a7
Iringo <iringobalazs@gmail.com>
2018-09-11 16:05:12 +0200

    feat (exercise bmi): finish task

---
+++ week-06/day1/bmi/main.c
@@ -0,0 +1,10 @@
+main()
+{
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+
+    // Print the Body mass index (BMI) based on these values
+
+    printf("The body mass index (BMI) is: %.2f", massInKg / (heightInM * heightInM));
+
+}
