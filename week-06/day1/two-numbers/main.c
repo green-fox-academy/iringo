a1b6e1e730ae5b6c5981022ee3cbced6869ece83
Iringo <iringobalazs@gmail.com>
2018-09-11 10:10:07 +0200

    feat (exercise two numbers) task finished

---
+++ week-06/day1/two-numbers/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+  // Create a program that prints a few operations on two numbers: 22 and 13
+
+  // Print the result of 13 added to 22
+
+  // Print the result of 13 substracted from 22
+
+  // Print the result of 22 multiplied by 13
+
+  // Print the result of 22 divided by 13 (as a decimal fraction)
+
+  // Print the reminder of 22 divided by 13
+
+    printf("%d \n", 13 + 22);
+    printf("%d \n", 13 - 22);
+    printf("%d \n", 22 * 13);
+    printf("%.2f \n", (22.00 / 13.00));
+    printf("%d \n", 22 % 13);
+
+  return 0;
+
+}
