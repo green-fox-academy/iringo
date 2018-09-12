7a88aada384d739313b2170ce5d5b4da6eef5559
Iringo <iringobalazs@gmail.com>
2018-09-12 20:43:09 +0200

    feat (exercise sum elements): finish task

---
+++ week-06/day2_3/SumElements/SumElements/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // - Create an array variable named `r`
+    //   with the following content: `[54, 23, 66, 12]`
+    // - Print the sum of the second and the third element
+    
+    int r[] = {54, 23, 66, 12};
+    printf ("%d\n", r[1] + r[2]);
+}
