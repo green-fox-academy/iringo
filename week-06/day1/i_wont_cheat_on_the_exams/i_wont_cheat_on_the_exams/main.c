2e058c93c564fbe4980fa75af21d79f13772a2f7
Iringo <iringobalazs@gmail.com>
2018-09-12 14:54:43 +0200

    feat (exercise: i will not cheat on the exam): finish task

---
+++ week-06/day1/i_wont_cheat_on_the_exams/i_wont_cheat_on_the_exams/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    
+    for (int i = 0; i <= 99; i++) {
+        printf("I won't cheat on the exam!\n");
+    }
+}
