fb7a2ecc1d90259575620331945aff1b5cd318a2
Iringo <iringobalazs@gmail.com>
2018-09-12 17:05:07 +0200

    feat (exercise: fizz buzz switch) task finished

---
+++ week-06/day1/fizzBuzzSwitch/fizzBuzzSwitch/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // Write a program that prints the numbers from 1 to 100.
+    // But for multiples of three print “Fizz” instead of the number
+    // and for the multiples of five print “Buzz”.
+    // For numbers which are multiples of both three and five print “FizzBuzz”.
+    // But this time use only switch case!
+    
+    for (int i = 1; i <= 100; i++) {
+        switch(i % 3) {
+            case 0:
+                switch (i % 5) {
+                case 0:
+                    printf("FizzBuzz\n");
+                    break;
+                default:
+                    printf("Fizz\n");
+                    break;
+                }
+                break;
+            default:
+                 switch (i % 5 ) {
+                 case 0:
+                    printf("Buzz\n");
+                    break;
+                 default:
+                    printf("%d \n", i);
+                    break;
+                }
+            break;
+        }
+    }
+}
+
