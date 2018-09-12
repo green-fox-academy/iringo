9741537cd19f1e45e776e78f1c3f1f83e206e4c1
Iringo <iringobalazs@gmail.com>
2018-09-12 13:10:35 +0200

    feat (exercise: hello user) finish task

---
+++ week-06/day1/HelloUser/HelloUser/main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   
+    // Modify this program int he printf() to greet user instead of the World!
+    // The program should ask for the age of the user
+    int age;
+    printf("Hello, users of the World! What is your age? \n");
+    scanf("%d", &age);
+    printf("%d\n", age);
+    
+}
