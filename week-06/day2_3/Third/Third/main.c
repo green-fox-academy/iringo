f59fad01de2ba2ec83199f8c4456322670b63a50
Iringo <iringobalazs@gmail.com>
2018-09-12 20:31:35 +0200

    feat (exercise third) task finished

---
+++ week-06/day2_3/Third/Third/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+    
+    char q[] = {4, 5, 6, 7};
+    printf("%d\n", q[2]);
+}
