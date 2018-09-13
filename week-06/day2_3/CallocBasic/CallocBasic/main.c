003da0c18f98777c35d13efad25a294b939b3617
Iringo <iringobalazs@gmail.com>
2018-09-13 13:47:04 +0200

    feat (exercise: calloc basic) finish task

--- week-06/day2_3/CallocBasic/CallocBasic/main.c
+++ week-06/day2_3/CallocBasic/CallocBasic/main.c
@@ -1,15 +1,24 @@
-//
-//  main.c
-//  CallocBasic
-//
-//  Created by Balázs Iringó on 2018. 09. 13..
-//  Copyright © 2018. Balázs Iringó. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+// with calloc
+// please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
+// please delete the array before the program exits
+// what's the difference between this and the previous excersice?
+
+int main() {
+    int* pointer = NULL;
+    pointer = (int*) calloc(5, sizeof(int));
+    for (int i = 0; i < 5; i++) {
+        printf("%d, ", pointer[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++) {
+        pointer[i] = i;
+        printf("%d, ", pointer[i]);
+    }
+    free(pointer);
     return 0;
+    
+    // The difference between maloc and calloc is on the one hand their syntax. Secondly, the malloc initializes the memory, while the calloc leaves the memory uninitialized.
 }
