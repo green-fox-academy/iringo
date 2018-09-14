eb4b77a2f1039005db7444725581a70999c76c7f
Iringo <iringobalazs@gmail.com>
2018-09-14 14:20:18 +0200

    feat (exercise count lines): finish task)

--- week-06/day4_5/CountLines/CountLines/main.c
+++ week-06/day4_5/CountLines/CountLines/main.c
@@ -1,15 +1,37 @@
-//
-//  main.c
-//  CountLines
-//
-//  Created by Balázs Iringó on 2018. 09. 14..
-//  Copyright © 2018. Balázs Iringó. All rights reserved.
-//
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file
 
 #include <stdio.h>
 
+int countNumberOfLines (char* filename);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    char* filename = "CountLines/file.txt";
+    FILE *fPointer = fopen(filename, "w");
+    fprintf(fPointer, "I love dogs.\n");
+    fprintf(fPointer, "I love dogs very much!\n");
+    fclose(fPointer);
+    printf("%d\n",countNumberOfLines(filename));
 }
+
+int countNumberOfLines (char* filename)
+{
+    int counter = 1;
+    char ch;
+    FILE *fpointer = fopen(filename, "r");
+    if (fpointer == NULL) {
+        printf("Could not open the file!");
+        return 0;
+    } else {
+        while((ch = fgetc(fpointer)) !=  EOF) {
+            if(ch == '\n') {
+                counter++;
+            }
+        }
+    }
+    fclose(fpointer);
+    return counter;
+}
+
+
