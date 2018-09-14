4f682cda8dbc180942559482f590708b14eb69ce
Iringo <iringobalazs@gmail.com>
2018-09-14 09:37:16 +0200

    feat (exercise: append a): finish task

--- week-06/day2_3/AppendA/AppendA/main.c
+++ week-06/day2_3/AppendA/AppendA/main.c
@@ -1,15 +1,27 @@
-//
-//  main.c
-//  AppendA
-//
-//  Created by Balázs Iringó on 2018. 09. 13..
-//  Copyright © 2018. Balázs Iringó. All rights reserved.
-//
+// - Create a char array variable named `typo` and assign the value `Chinchill` to it
+// - Write a function called `appendAFunc` that gets a string as an input,
+//   appends an 'a' character to its end and returns with a string
+// - Print the result of `appendAFunc(typo)`
 
 #include <stdio.h>
+#include <stdlib.h>
+
+char* appendAFunc(char* input);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    char* typo = "Chinchill";
+    printf ("%s\n", appendAFunc(typo));
+}
+
+char* appendAFunc(char* input)
+{
+    int i;
+    char* outputArray = (char*) calloc (sizeof(*input) + 1, sizeof(char));
+    for (int i = 0; input[i] != 0; i++) {
+        outputArray[i] = input[i];
+    }
+    for(i = 0; outputArray[i] != '\0'; ++i);
+    outputArray[i] = 'a';
+    outputArray[i + 1] = '\0';
+    return outputArray;
 }
