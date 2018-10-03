f82760f6c051c733930ffd64998c6f7e22dc3588
Iringo <iringobalazs@gmail.com>
2018-10-03 11:36:57 +0200

    feat (exercise: countS) finish task

--- week-08/TrialExam/CountS/CountS/main.c
+++ week-08/TrialExam/CountS/CountS/main.c
@@ -1,15 +1,35 @@
-//
-//  main.c
-//  CountS
-//
-//  Created by Balázs Iringó on 2018. 10. 02..
-//  Copyright © 2018. Balázs Iringó. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ends_Withs_S (char* filename);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    printf("%d\n", ends_Withs_S("text.txt"));
+}
+
+int ends_Withs_S (char* filename)
+{
+    FILE *fPointer = fopen(filename, "r");
+    int counter = 0;
+    if (fPointer == NULL) {
+        printf("Could not open the file!\n");
+    } else {
+        char buffer[1000];
+         while ((fgets(buffer, 1000, fPointer)) != NULL) {
+         }
+         for (int i = 0; i < sizeof(buffer)/sizeof(char); i++) {
+            if (buffer[i] == ' ' || buffer[i] == '\n') {
+                if (buffer[i-1] == 's') {
+                    counter++;
+                } else if (buffer[i-1] == '.' || buffer[i-1] == ',' || buffer[i-1] == ';' ||buffer[i-1] == ':') {
+                    if (buffer[i-2] == 's') {
+                        counter++;
+                    }
+                }
+            }
+         }
+    }
+    fclose(fPointer);
+    return counter;
 }
